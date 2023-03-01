6bc55ada0666939444612ed688cee88a1557a57a
Zacharia <ndunguzachary24@gmail.com>
2023-03-01 14:04:32 +0300

    Service padding

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -45,10 +45,10 @@ const Hero = () => {
         src={school}        class="block w-full h-[400px]  md:h-[700px]"
         alt="..." />
       <div
-        class="absolute inset-x-[15%] bottom-80 py-5   text-blue-400  md:block">
-        <h5 class="text-8xl font-bold  font-serif">Soma Loan</h5>
-              <p class="text-4xl font-semibold font-mono">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ""
+        class="absolute inset-x-[15%] bottom-16 md:bottom-80 py-5   text-blue-900  md:block">
+        <h5 class="text-4xl md:text-6xl font-bold  font-serif">Soma Loan</h5>
+              <p class="text-lg md:text-4xl font-semibold font-mono">
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit,""
         </p>
       </div>
     </div>
@@ -61,9 +61,9 @@ const Hero = () => {
         class="block w-full h-[400px]  md:h-[700px]"
         alt="..." />
       <div
-        class="absolute inset-x-[15%] bottom-46 py-5   text-blue-400 md:block">
-        <h5 class="text-4xl font-bold font-serif">Business Loan</h5>
-        <p class="text-4xl font-semibold font-mono">
+        class="absolute inset-x-[15%] bottom-16 md:bottom-80 py-5   text-black md:block">
+        <h5 class="text-4xl md:text-6xl font-bold font-serif">Business Loan</h5>
+        <p class="text-lg md:text-4xl font-semibold font-mono">
           Some representative placeholder content for the third slide.
         </p>
       </div>
@@ -77,9 +77,9 @@ const Hero = () => {
         class="block w-full h-[400px]  md:h-[700px]"
         alt="..." />
       <div
-        class="absolute inset-x-[15%] bottom-80 py-5  text-blue-400 md:block">
-        <h5 class="text-8xl font-bold font-serif">Farming Loans</h5>
-        <p class="text-4xl font-semibold font-mono">
+        class="absolute inset-x-[15%] bottom-16 md:bottom-80 py-5  text-white md:block">
+        <h5 class="text-4xl md:text-6xl font-bold font-serif">Farming Loans</h5>
+        <p class="text-lg md:text-4xl font-semibold font-mono">
           Some representative placeholder content for the third slide.
         </p>
       </div>
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -3,18 +3,18 @@ import { busines, farm, school } from '../img'
 
 const Services = () => {
   return (
-    <section className=' md:container md:mx-auto px-16 pt-6'>
+    <section className=' md:container md:mx-auto px-5 pt-4'>
       <div>
         <h1 className='text-center font-bold text-3xl md:text-4xl underline pb-4'>Our Services</h1>
       </div>
-        <div className=" grid md:grid-cols-1  md:px-8 pb-4">
+        <div className=" grid md:grid-cols-1  md:px-8 pb-6">
         <div className="flex flex-col  float-right  bg-white shadow-md  lg:flex-row items-center">
           <div className="flex flex-1 justify-center items-center ">
-            <img className="max-w-auto md:max-w-[600px] rounded-xl" src={school} alt="" />
+            <img className="max-w-auto md:max-w-[600px] rounded-xl" src={farm} alt="" />
           </div>
           <div className="flex-1 text-center lg:text-left">
             <h1 className="text-[26px] font-bold mb-2 max-w-[45 0px] mx-auto text-blue-400">
-              Soma Loan 
+            Farming Loans 
             </h1>
             <p className="text-sm">
             ""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat .  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." "
@@ -22,23 +22,23 @@ const Services = () => {
           </div>
         </div>
       </div>
-      <div className=" grid md:grid-cols-1  md:px-8 pb-4">
+      <div className=" grid md:grid-cols-1  md:px-8 pb-6">
         <div className="flex flex-col  float-right  bg-white shadow-md  lg:flex-row items-center">
         <div className="flex-1 text-center lg:text-left">
             <h1 className="text-[26px] font-bold mb-2 max-w-[45 0px] mx-auto text-blue-400">
-           Farming Loans
+        Soma Loans
             </h1>
             <p className="text-sm">
             ""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat .  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." "
             </p>
           </div>
           <div className="flex flex-1 justify-center items-center ">
-            <img className="max-w-auto md:max-w-[600px] rounded-xl" src={farm} alt="" />
+            <img className="max-w-auto md:max-w-[600px] rounded-xl" src={school} alt="" />
           </div>
          
         </div>
       </div>
-      <div className=" hidden  md:grid md:grid-cols-1 md:px-8 pb-4">
+      <div className=" hidden  md:grid md:grid-cols-1 md:px-8 pb-6">
         <div className="flex flex-col  float-right  bg-white shadow-md  lg:flex-row items-center">
           <div className="flex flex-1 justify-center items-center ">
             <img className="max-w-auto md:max-w-[600px] rounded-xl" src={busines} alt="" />
