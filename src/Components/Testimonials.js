10dab61c80ad80ecc56e55fa9f58ed29835830b5
Zacharia <ndunguzachary24@gmail.com>
2023-03-02 17:14:28 +0300

    Clients' testimonials

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -3,13 +3,14 @@ import { james } from '../img'
 
 const Testimonials = () => {
   return (
-    <section className='md:h-[300px] bg-gray-400 grid  grid-cols-1 place-items-center container '>
-        <div className="flex  gap-2   justify-between place-items-center bg-gray-200 shadow-xl ">
+    <section className='md:h-[300px] bg-gray-200 grid  grid-cols-1 place-items-center container '>
+         <h2 className='text-lg md:text-xl font-bold '>Clients'  Testimonials </h2>
+      <div className="flex  gap-2   justify-between place-items-center bg-gray-200 shadow-xl ">
           <div className="flex-1 justify-center m-2 ">
             <img className="max-w-[150px] md:max-w-[300px] rounded-full" src={james} alt="" />
           </div>
           <div className=" flex-1  ">
-            <h1 className=" font-bold ">
+            <h1 className=" font-bold  text-blue-400">
        James Peterson Mwangi 
             </h1>
             <p  className='underline'>Business Man </p>
